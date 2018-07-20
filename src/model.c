7ac8c93e5d7a8ad7d6d93518ca70a63899f5a0ae
Martin Biely <github@biely.eu>
2018-07-20 21:38:56 +0200

    scheisse fixen

--- src/model.c
+++ src/model.c
@@ -1,20 +1,20 @@
 #include "model.h"
-
-#define MatrixDataSize(resolution) resolution * resolution * resolution, sizeof(voxel_t)
+#include "memory.h"
 
 matrix_t
 make_matrix(int resolution) {
     matrix_t m;
     m.resolution = resolution;
-    m.data = calloc(MatrixDataSize(resolution);
+    m.data = calloc(resolution * resolution * resolution, sizeof(voxel_t));
     return m;
 }
 
 matrix_t
 copy_matrix(matrix_t from) {
     matrix_t m = make_matrix(from.resolution);
-    memcpy(m.data, from.data, MatrixDataSize(resolution);
+    memcpy(m.data, from.data, from.resolution * from.resolution * from.resolution * sizeof(voxel_t));
     return m;
+}
 
 void
 free_matrix (matrix_t matrix) {
