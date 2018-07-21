ea5f9830bf0dc5e8a4fa790281e6ef6b24280447
Heinz Deinhart <heinz.deinhart@tuwien.ac.at>
2018-07-21 12:15:23 +0200

    make code less russian

--- generate_default_traces.sh
+++ generate_default_traces.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-MODELS=`cd lightning_problems; ls *mdl`
+MODELS=$(cd lightning_problems; ls -- *mdl)
 
 make -C src generate_default_trace
 make -C Bertl ascii2nbt
 for file in $MODELS; do
-	echo Converting $file to submission/Traces/${file/_tgt.mdl/.nbt}
-	src/generate_default_trace lightning_problems/$file | Bertl/ascii2nbt > submission/Traces/${file/_tgt.mdl/.nbt}
+	echo "Converting $file to submission/Traces/${file/_tgt.mdl/.nbt}"
+	src/generate_default_trace "lightning_problems/$file" | \
+	    Bertl/ascii2nbt > "submission/Traces/${file/_tgt.mdl/.nbt}"
 done
