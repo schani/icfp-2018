8c95099ea5a41f60ea730dc4e0d5011cd4730049
Alexander Kößler <koe@itcorner.net>
2018-07-21 15:23:01 +0200

    cleanup

--- src/potential.h
+++ src/potential.h
@@ -6,16 +6,14 @@
 #include <gmodule.h>
 #include "area_detect.h"
 
-typedef struct{
-
- matrix_t m;
- GArray * elements;
-
-} area_t;
 
 
-
-
+int 
+calc_potential_simple(matrix_t * m_grounded, matrix_t * phases, matrix_t * blobs, int n_blob, int n_phase, int y);
+int 
+calc_num_grounded(matrix_t * m_grounded, matrix_t * phases, matrix_t * blobs, int n_blob, int n_phase, int y);
+GArray * 
+get_frontier(matrix_t * m_grounded, matrix_t * phases, matrix_t * blobs, int n_blob, int n_phase, int y);
 int calc_potential(matrix_t * m_grounded, matrix_t * phases, matrix_t * blobs, int n_blob, int n_phase, int y);
 
 #endif /* _POTENTIAL_H */
