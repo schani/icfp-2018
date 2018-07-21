23b3c26dd0d48db499cbbe2b66415f0130523870
Heinz Deinhart <heinz.deinhart@tuwien.ac.at>
2018-07-21 12:05:32 +0200

    make sed work on macs (needs gsed installed)

---
+++ src/sed.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SEDLIST=(/usr/local/bin/gsed /bin/sed)
+
+for SED in "${SEDLIST[@]}"
+do
+    if [ -x "$SED" ]
+    then
+	"$SED" "$@"
+	exit $?
+    fi
+done
+
+echo "$0: FATAL ERROR: sed not found" >&2
+exit 1
