a0d599c82c35898d6430b5b5d34b4d1c050eb2dd
Dominik Macher <dominikmacher@live.at>
2018-07-21 16:56:57 +0200

    compute potential automatically in test

--- src/_test_blobs.c
+++ src/_test_blobs.c
@@ -8,6 +8,7 @@
 #include "trace.h"
 #include "plan.h"
 #include "potential.h"
+#include "area.h"
 
 
 void fill_rectangle_2d(matrix_t *m, int x_start, int y_fixed, int z_start, int x_len, int z_len) {
@@ -31,17 +32,20 @@ main()
     voxel_t tmp[10*10*10] = {0};
     testModel.data = tmp;
    
-    set_voxel(&testModel, create_coord(1,0,1), Full);
+    //set_voxel(&testModel, create_coord(1,0,1), Full);
     // filled rectangle #1
-    fill_rectangle_2d(&testModel,1,1,1,2,2);
+    //fill_rectangle_2d(&testModel,1,1,1,2,2);
+    fill_rectangle_2d(&testModel,1,0,1,4,4);
+    fill_rectangle_2d(&testModel,4,1,2,2,4);
+    fill_rectangle_2d(&testModel,1,1,3,1,3);
     // filled rectangle #2
-    fill_rectangle_2d(&testModel,4,1,4,3,3);
+    //fill_rectangle_2d(&testModel,4,1,4,3,3);
     //connect rectangles
-    set_voxel(&testModel, create_coord(2,2,2), Full);
+    /*set_voxel(&testModel, create_coord(2,2,2), Full);
     set_voxel(&testModel, create_coord(3,2,2), Full);
     set_voxel(&testModel, create_coord(4,2,2), Full);
     set_voxel(&testModel, create_coord(4,2,3), Full);
-    set_voxel(&testModel, create_coord(4,2,4), Full);
+    set_voxel(&testModel, create_coord(4,2,4), Full);*/
 
 
     matrix_t phases = make_matrix(res);
@@ -50,36 +54,31 @@ main()
     make_plan(&testModel, &phases, &blobs);
     make_grounded(&phases, &grounded);
 
-    int fixed_y = 1;
     int fixed_phase = 1;
+    int max_y = 0;
 
     region_t r = matrix_region(&phases);
     FOR_EACH_COORD(c, r) {
+        if (get_voxel(&phases, c) > 0) {
         //if (get_voxel(&phases, c) == fixed_phase) {
-          //  if (fixed_y == c.y)
-                
-                printf("Coordinate (%i,%i,%i): [ PHASE | BLOB | GROUNDED ] --> [ %d | %d | %d ]\n", c.x, c.y, c.z, get_voxel(&phases, c), get_voxel(&blobs, c), get_voxel(&grounded, c)); 
-                
-        //}
-    } END_FOR_EACH_COORD;
-
-  printf("woran liegts\n");
-    // iterate through phases and blobs, calc potential
-    
-    /*for (int x = 0; x < res; x++) {
-        for (int z = 0; z < res; z++) {
-            coord_t c = create_coord(x, fixed_y, z);
-            if (get_voxel(c, phases, fixed_phase) != 0) { 
-                int pot = calc_potential(m_grounded, phases, blobs, int n_blob, int n_phase, int y)
+            printf("Coordinate (%i,%i,%i): [ PHASE | BLOB | GROUNDED ] --> [ %d | %d | %d ]\n", c.x, c.y, c.z, get_voxel(&phases, c), get_voxel(&blobs, c), get_voxel(&grounded, c)); 
+            if (c.y > max_y) {
+                max_y = c.y;
             }
         }
-    }*/
-    printf("bis hierher 222?\n");
+    } END_FOR_EACH_COORD;
 
-    calc_potential(&grounded, &phases, &blobs, 1, fixed_phase, fixed_y);
-
-    //printf("Potential --> %d\n", ); 
-
+    // iterate through phases and blobs, calc potential
+    for (int y=0; y<=max_y; y++) {
+        GArray* areas = find_areas(&phases, &blobs, fixed_phase, y);
+        for (int a=0; a<areas->len; a++) {
+            area_t area = g_array_index(areas, area_t, a);
+            int pot = calc_potential(&grounded, &phases, &blobs, a+1, fixed_phase, y);
+            area.potential = pot;
+            printf("Potential for y-Layer %d / Blob %d : %d\n", y, a+1, pot); 
+        }
+    }
+    
 
     return 0;
 }
