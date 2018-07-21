95bd75b449f4dd276453fa0563b1608b2fa9c5ac
Alexander Kößler <koe@itcorner.net>
2018-07-21 17:23:02 +0200

    make_plan now returns number of phases

--- src/plan.h
+++ src/plan.h
@@ -9,7 +9,9 @@ typedef enum{
     TransitiveGrounded
 } grounded_type_t;
 
-void make_plan (matrix_t *model, matrix_t *phases, matrix_t *blobs);
+
+/* returns number of phases */
+int make_plan (matrix_t *model, matrix_t *phases, matrix_t *blobs);
 
 void make_grounded (matrix_t *phases, matrix_t* grounded);
 
