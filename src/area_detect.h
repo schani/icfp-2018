a645effb2716cf9b174b0b5047558816e2b66c37
Dominik Macher <dominikmacher@live.at>
2018-07-21 12:58:29 +0200

    added area + extd_coords_t

---
+++ src/area_detect.h
@@ -0,0 +1,27 @@
+#ifndef _AREA_H
+#define _AREA_H
+
+#include "coord.h"
+
+
+typedef struct {
+    coord_t c;
+    int grounded;
+    int potential;
+} extd_coord_t;
+
+
+
+/**
+ * Input: MATRIX, y-Ebene fix
+ * Output: Liste an Flächen
+ */
+GArray* identify_areas(matrix_t *xzArea) {
+    
+    GArray *coords = g_array_new(FALSE, FALSE, sizeof(extd_coord_t));
+
+    return coords;
+}
+
+
+#endif /* _AREA_H */
