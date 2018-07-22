6e4a15747d89e03574c4c60293c3ca2efa66910a
Herbert Poetzl <herbert@13thfloor.at>
2018-07-22 22:37:50 +0200

    fixed range checks for conversion

--- src/nbtio.h
+++ src/nbtio.h
@@ -24,24 +24,24 @@ int lldtoai(int dx, int dy, int dz, int *a, int *i)
 	if (dx && (dy == 0) && (dz == 0)) {
 	    *a = 1;
 
-	    if ((dx > -15) && (dx < 15))
-		*i = dx + 15;
-	    else
+	    if ((dx < -15) && (dx > 15))
 		return -2;
+	    else
+		*i = dx + 15;
 	} else if (dy && (dx == 0) && (dz == 0)) {
 	    *a = 2;
 
-	    if ((dy > -15) && (dy < 15))
-		*i = dy + 15;
-	    else
+	    if ((dy < -15) && (dy > 15))
 		return -2;
+	    else
+		*i = dy + 15;
 	} else if (dz && (dx == 0) && (dy == 0)) {
 	    *a = 3;
 
-	    if ((dz > -15) && (dz < 15))
-		*i = dz + 15;
-	    else
+	    if ((dz < -15) && (dz > 15))
 		return -2;
+	    else
+		*i = dz + 15;
 	} else 
 	    return -1;
 	return 0;
@@ -53,24 +53,24 @@ int sldtoai(int dx, int dy, int dz, int *a, int *i)
 	if (dx && (dy == 0) && (dz == 0)) {
 	    *a = 1;
 
-	    if ((dx > -5) && (dx < 5))
-		*i = dx + 5;
-	    else
+	    if ((dx < -5) || (dx > 5))
 		return -2;
+	    else
+		*i = dx + 5;
 	} else if (dy && (dx == 0) && (dz == 0)) {
 	    *a = 2;
 
-	    if ((dy > -5) && (dy < 5))
-		*i = dy + 5;
-	    else
+	    if ((dy < -5) && (dy > 5))
 		return -2;
+	    else
+		*i = dy + 5;
 	} else if (dz && (dx == 0) && (dy == 0)) {
 	    *a = 3;
 
-	    if ((dz > -5) && (dz < 5))
-		*i = dz + 5;
-	    else
+	    if ((dz < -5) && (dz > 5))
 		return -2;
+	    else
+		*i = dz + 5;
 	} else 
 	    return -1;
 	return 0;
