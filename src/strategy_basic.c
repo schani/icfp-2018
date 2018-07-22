a563f7f86b00bc990f66964a3acea02d8106058a
Alexander Kößler <koe@itcorner.net>
2018-07-22 18:39:43 +0200

    added multibot-oneshot clearence functions

--- src/strategy_basic.c
+++ src/strategy_basic.c
@@ -71,13 +71,29 @@ calc_boundary_box_in_region(matrix_t *mdl, region_t r, region_t* bb){
     return num_filled;
 }
 
-
+typedef struct{
+    bot_commands_t *b1;
+    bot_commands_t *b2;
+} bc2_t;
 
 
 typedef struct{
-    GArray * b1_cmds;
-    GArray * b2_cmds;
-} cmd2;
+    bot_commands_t *b1;
+    bot_commands_t *b2;
+    bot_commands_t *b3;
+    bot_commands_t *b4;
+} bc4_t;
+
+typedef struct{
+    bot_commands_t *b1;
+    bot_commands_t *b2;
+    bot_commands_t *b3;
+    bot_commands_t *b4;
+    bot_commands_t *b5;
+    bot_commands_t *b6;
+    bot_commands_t *b7;
+    bot_commands_t *b8;
+} bc8_t;
 
 typedef struct{
     GArray * b1_cmds;
@@ -85,19 +101,6 @@ typedef struct{
     GArray * b3_cmds;
     GArray * b4_cmds;
 } cmd4;
-
-typedef struct{
-    GArray * b1_cmds;
-    GArray * b2_cmds;
-    GArray * b3_cmds;
-    GArray * b4_cmds;
-    GArray * b5_cmds;
-    GArray * b6_cmds;
-    GArray * b7_cmds;
-    GArray * b8_cmds;
-} cmd8;
-
-
 
 /* assumes the 4 bots are sitting on the top of the bb 
    b1 over c_min. b2, b3, b4 oriented clockwise
@@ -180,12 +183,6 @@ void_a_boundary_box(matrix_t *mdl, region_t* bb){
 
 
 
-cmd8
-void_a_small_boundary_box(matrix_t *mdl, region_t* bb){
-
-
-
-}
 
 
 
@@ -201,29 +198,229 @@ void wait_n_rounds(GArray *cmds, int n){
     }
 }
 
+
+/*
 void spawn_at_pos(GArray *cmds, coord_t spawn_vect, int seed_m){
     add_cmd(cmds, fission_cmd(spawn_vect, seed_m));
 }
 
+*/
 
-cmd2
-move_b2_to_pos_and_duplicate_bots(matrix_t *mdl, coord_t b1_src, coord_t b2_src, coord_t b2_target, coord_t spawn_vect, int seed_m){
-    cmd2 ret_val;
 
-	GArray *b1_cmds = g_array_new(FALSE, FALSE, sizeof(command_t));
-	GArray *b2_cmds = g_array_new(FALSE, FALSE, sizeof(command_t));
 
-    ret_val.b1_cmds = b1_cmds;
-    ret_val.b2_cmds = b2_cmds;
+bc2_t
+initial_spawn(matrix_t *mdl, bot_commands_t *b1, coord_t spawn_vect){
+    bc2_t ret_val;
+    ret_val.b1 = b1;
 
-    goto_next_pos(&b2_src, b2_target, b2_cmds);
+    /* spawn the new bot */
+    *ret_val.b2 = fission(b1, spawn_vect);
 
-    wait_n_rounds(b1_cmds, b2_cmds->len);
-    spawn_at_pos(b1_cmds, spawn_vect, seed_m);
-    spawn_at_pos(b2_cmds, spawn_vect, seed_m);
-    
     return ret_val;
 }
+
+bc4_t
+move_b2_to_pos_and_duplicate(matrix_t *mdl, bc2_t bot_commands, coord_t b2_target, coord_t spawn_vect){
+    bc4_t ret_val;
+
+    ret_val.b1 = bot_commands.b1;
+    ret_val.b2 = bot_commands.b2;
+
+
+    /* move bot 2 to correct position and keep bot 1 waiting */
+    int wait_time = goto_rel_pos(sub_coords(b2_target, get_bot_pos(bot_commands.b2)), bot_commands.b2->cmds);
+    set_bot_pos(bot_commands.b2, b2_target);
+    wait_n_rounds(bot_commands.b1->cmds, wait_time);
+
+    /* spawn the new bots */
+    *ret_val.b3 = fission(bot_commands.b1, spawn_vect);
+    *ret_val.b4 = fission(bot_commands.b2, spawn_vect);
+
+    return ret_val;
+}
+
+bc8_t
+move_b3b4_to_pos_and_duplicate(matrix_t *mdl, bc4_t bot_commands, coord_t b3_target, coord_t b4_target, coord_t spawn_vect){
+    bc8_t ret_val;
+
+    ret_val.b1 = bot_commands.b1;
+    ret_val.b2 = bot_commands.b2;
+    ret_val.b3 = bot_commands.b3;
+    ret_val.b4 = bot_commands.b4;
+
+    int wait_time;
+    /* move bot 3 and bot 4 to correct position and keep bot 1 and bot 2 waiting */
+    int wait_time3 = goto_rel_pos(sub_coords(b3_target, get_bot_pos(bot_commands.b3)), bot_commands.b3->cmds);
+    set_bot_pos(bot_commands.b3, b3_target);
+    int wait_time4 = goto_rel_pos(sub_coords(b4_target, get_bot_pos(bot_commands.b4)), bot_commands.b4->cmds);
+    set_bot_pos(bot_commands.b4, b4_target);
+    
+    wait_time = MAX(wait_time3, wait_time4);
+
+    if(wait_time3 < wait_time){
+        wait_n_rounds(ret_val.b3->cmds, wait_time-wait_time3);
+    }
+    if(wait_time4 < wait_time){
+        wait_n_rounds(ret_val.b4->cmds, wait_time-wait_time4);
+    }
+
+    wait_n_rounds(bot_commands.b1->cmds, wait_time);
+    wait_n_rounds(bot_commands.b2->cmds, wait_time);
+
+    /* spawn the new bots */
+    *ret_val.b5 = fission(bot_commands.b1, spawn_vect);
+    *ret_val.b6 = fission(bot_commands.b2, spawn_vect);
+    *ret_val.b7 = fission(bot_commands.b3, spawn_vect);
+    *ret_val.b8 = fission(bot_commands.b4, spawn_vect);
+
+    return ret_val;
+
+}
+
+void
+move_b5b6b7b8_to_pos_and_void(matrix_t *mdl, bc8_t bot_commands, coord_t b5_target, coord_t b6_target, coord_t b7_target, coord_t b8_target){
+    int wait_time;
+    int wait_time_a;
+    int wait_time_b;
+
+    /* move bot 5, bot 6, bot 7, and bot 8 to correct position and keep others waiting */
+    int wait_time5 = goto_rel_pos(sub_coords(b5_target, get_bot_pos(bot_commands.b5)), bot_commands.b5->cmds);
+    set_bot_pos(bot_commands.b5, b5_target);
+    int wait_time6 = goto_rel_pos(sub_coords(b6_target, get_bot_pos(bot_commands.b6)), bot_commands.b6->cmds);
+    set_bot_pos(bot_commands.b6, b6_target);
+    int wait_time7 = goto_rel_pos(sub_coords(b7_target, get_bot_pos(bot_commands.b7)), bot_commands.b7->cmds);
+    set_bot_pos(bot_commands.b7, b7_target);
+    int wait_time8 = goto_rel_pos(sub_coords(b8_target, get_bot_pos(bot_commands.b8)), bot_commands.b8->cmds);
+    set_bot_pos(bot_commands.b8, b8_target);
+
+    wait_time_a = MAX(wait_time5, wait_time6);
+    wait_time_b = MAX(wait_time7, wait_time8);
+    wait_time = MAX(wait_time_a, wait_time_b);
+    
+    if(wait_time5 < wait_time){
+        wait_n_rounds(bot_commands.b5->cmds, wait_time-wait_time5);
+    }
+    if(wait_time6 < wait_time){
+        wait_n_rounds(bot_commands.b6->cmds, wait_time-wait_time6);
+    }
+    if(wait_time7 < wait_time){
+        wait_n_rounds(bot_commands.b7->cmds, wait_time-wait_time7);
+    }
+    if(wait_time8 < wait_time){
+        wait_n_rounds(bot_commands.b8->cmds, wait_time-wait_time8);
+    }
+
+    wait_n_rounds(bot_commands.b1->cmds, wait_time);
+    wait_n_rounds(bot_commands.b2->cmds, wait_time);
+    wait_n_rounds(bot_commands.b3->cmds, wait_time);
+    wait_n_rounds(bot_commands.b4->cmds, wait_time);
+
+    /* void the region surrounded */
+    add_cmd(bot_commands.b1->cmds, gvoid_cmd(create_coord( 1, 0,  1), create_coord( mdl->resolution-3,  mdl->resolution-1,  mdl->resolution-3)));
+    add_cmd(bot_commands.b8->cmds, gvoid_cmd(create_coord(-1, 0, -1), create_coord(-mdl->resolution-3, -mdl->resolution-1, -mdl->resolution-3)));
+
+    add_cmd(bot_commands.b2->cmds, gvoid_cmd(create_coord(-1, 0,  1), create_coord(-mdl->resolution-3,  mdl->resolution-1,  mdl->resolution-3)));
+    add_cmd(bot_commands.b7->cmds, gvoid_cmd(create_coord( 1, 0, -1), create_coord( mdl->resolution-3, -mdl->resolution-1, -mdl->resolution-3)));
+
+    add_cmd(bot_commands.b3->cmds, gvoid_cmd(create_coord( 1, 0, -1), create_coord( mdl->resolution-3,  mdl->resolution-1, -mdl->resolution-3)));
+    add_cmd(bot_commands.b6->cmds, gvoid_cmd(create_coord(-1, 0,  1), create_coord(-mdl->resolution-3, -mdl->resolution-1,  mdl->resolution-3)));
+
+    add_cmd(bot_commands.b4->cmds, gvoid_cmd(create_coord(-1, 0, -1), create_coord(-mdl->resolution-3,  mdl->resolution-1, -mdl->resolution-3)));
+    add_cmd(bot_commands.b5->cmds, gvoid_cmd(create_coord( 1, 0,  1), create_coord( mdl->resolution-3, -mdl->resolution-1,  mdl->resolution-3)));
+
+}
+
+
+void
+move_b5b6b7b8_to_pos_and_fuse(matrix_t *mdl, bc8_t bot_commands, coord_t b5_target, coord_t b6_target, coord_t b7_target, coord_t b8_target){
+    int wait_time;
+    int wait_time_a;
+    int wait_time_b;
+
+    /* move bot 5, bot 6, bot 7, and bot 8 to correct position and keep others waiting */
+    int wait_time5 = goto_rel_pos(sub_coords(b5_target, get_bot_pos(bot_commands.b5)), bot_commands.b5->cmds);
+    set_bot_pos(bot_commands.b5, b5_target);
+    int wait_time6 = goto_rel_pos(sub_coords(b6_target, get_bot_pos(bot_commands.b6)), bot_commands.b6->cmds);
+    set_bot_pos(bot_commands.b6, b6_target);
+    int wait_time7 = goto_rel_pos(sub_coords(b7_target, get_bot_pos(bot_commands.b7)), bot_commands.b7->cmds);
+    set_bot_pos(bot_commands.b7, b7_target);
+    int wait_time8 = goto_rel_pos(sub_coords(b8_target, get_bot_pos(bot_commands.b8)), bot_commands.b8->cmds);
+    set_bot_pos(bot_commands.b8, b8_target);
+
+    wait_time_a = MAX(wait_time5, wait_time6);
+    wait_time_b = MAX(wait_time7, wait_time8);
+    wait_time = MAX(wait_time_a, wait_time_b);
+    
+    if(wait_time5 < wait_time){
+        wait_n_rounds(bot_commands.b5->cmds, wait_time-wait_time5);
+    }
+    if(wait_time6 < wait_time){
+        wait_n_rounds(bot_commands.b6->cmds, wait_time-wait_time6);
+    }
+    if(wait_time7 < wait_time){
+        wait_n_rounds(bot_commands.b7->cmds, wait_time-wait_time7);
+    }
+    if(wait_time8 < wait_time){
+        wait_n_rounds(bot_commands.b8->cmds, wait_time-wait_time8);
+    }
+
+    wait_n_rounds(bot_commands.b1->cmds, wait_time);
+    wait_n_rounds(bot_commands.b2->cmds, wait_time);
+    wait_n_rounds(bot_commands.b3->cmds, wait_time);
+    wait_n_rounds(bot_commands.b4->cmds, wait_time);
+
+    /* fuse the bots */
+    fusion(bot_commands.b1, bot_commands.b5);
+    fusion(bot_commands.b2, bot_commands.b6);
+    fusion(bot_commands.b3, bot_commands.b7);
+    fusion(bot_commands.b4, bot_commands.b8);
+
+}
+
+
+void
+move_b3b4_to_pos_and_fuse(matrix_t *mdl, bc4_t bot_commands, coord_t b3_target, coord_t b4_target, coord_t spawn_vect){
+
+    int wait_time;
+    /* move bot 3 and bot 4 to correct position and keep bot 1 and bot 2 waiting */
+    int wait_time3 = goto_rel_pos(sub_coords(b3_target, get_bot_pos(bot_commands.b3)), bot_commands.b3->cmds);
+    set_bot_pos(bot_commands.b3, b3_target);
+    int wait_time4 = goto_rel_pos(sub_coords(b4_target, get_bot_pos(bot_commands.b4)), bot_commands.b4->cmds);
+    set_bot_pos(bot_commands.b4, b4_target);
+    
+    wait_time = MAX(wait_time3, wait_time4);
+
+    if(wait_time3 < wait_time){
+        wait_n_rounds(bot_commands.b3->cmds, wait_time-wait_time3);
+    }
+    if(wait_time4 < wait_time){
+        wait_n_rounds(bot_commands.b4->cmds, wait_time-wait_time4);
+    }
+
+    wait_n_rounds(bot_commands.b1->cmds, wait_time);
+    wait_n_rounds(bot_commands.b2->cmds, wait_time);
+
+    /* fuse the bots */
+    fusion(bot_commands.b1, bot_commands.b3);
+    fusion(bot_commands.b2, bot_commands.b4);
+
+}
+
+
+void
+move_b2_to_pos_and_fuse(matrix_t *mdl, bc2_t bot_commands, coord_t b2_target, coord_t spawn_vect){
+    
+
+    /* move bot 2 to correct position and keep bot 1 waiting */
+    int wait_time = goto_rel_pos(sub_coords(b2_target, get_bot_pos(bot_commands.b2)), bot_commands.b2->cmds);
+    set_bot_pos(bot_commands.b2, b2_target);
+    wait_n_rounds(bot_commands.b1->cmds, wait_time);
+
+    /* fuse the bots */
+    fusion(bot_commands.b1, bot_commands.b2);
+
+}
+
 
 
 
@@ -233,10 +430,17 @@ GArray*
 exec_flush_at_once(matrix_t *mdl){
 	GArray *cmds = g_array_new(FALSE, FALSE, sizeof(command_t));
     /* spawn new bot one x coord ahead */
-    spawn_at_pos(cmds, create_coord(1,0,0), 3);
+    //spawn_at_pos(cmds, create_coord(1,0,0), 3);
 
     /* move new robot to end and spawn in z direction new robots */
-    cmd2 phase1_commands = move_b2_to_pos_and_duplicate_bots(mdl, create_coord(0,0,0), create_coord(1,0,0), create_coord(mdl->resolution-1,0,0),create_coord(0,0,1),2);
+    //cmd2 phase1_commands = move_b2_to_pos_and_duplicate_bots(mdl, create_coord(0,0,0), create_coord(1,0,0), create_coord(mdl->resolution-1,0,0),create_coord(0,0,1),2);
+
+
+
+
+
+
+
 
 }
 
